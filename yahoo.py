619d42354099527d31521f563670d8a47953f2f2
yc5 <yc@xstar.me>
2021-09-04 12:26:35 +0800

    add click top ten

--- yahoo.py
+++ yahoo.py
@@ -13,6 +13,8 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
 account_email = config('YAHOO_EMAIL')
 account_password = config('YAHOO_PW')
+YAHOO_T = config('YAHOO_T')
+YAHOO_Y = config('YAHOO_Y')
 
 t = time.localtime()
 current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
@@ -24,8 +26,8 @@ try:
     driver.get("https://mail.yahoo.com/")
     print("setting cookie ...")
     
-    driver.add_cookie({"name": "T", "value": config('YAHOO_T')})
-    driver.add_cookie({"name": "Y", "value": config('YAHOO_Y')})
+    driver.add_cookie({"name": "T", "value": YAHOO_T})
+    driver.add_cookie({"name": "Y", "value": YAHOO_Y})
     print("redirect to yahoo mail ...")
     driver.get("https://mail.yahoo.com/")
     
@@ -89,5 +91,33 @@ print("===")
 t = time.localtime()
 current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
 print("end:", current_time)
+
+time.sleep(5)
+try:
+    print("=== click top ten ===")
+    driver.get("https://tw.search.yahoo.com/search?p=%E5%88%BA%E5%88%BA%E6%98%9F&fr=yfp-search-sa")
+    print("setting cookie ...")
+
+    driver.add_cookie({"name": "T", "value": YAHOO_T})
+    driver.add_cookie({"name": "Y", "value": YAHOO_Y})
+    print("redirect to yahoo index ...")
+    driver.get("https://tw.search.yahoo.com/search?p=%E5%88%BA%E5%88%BA%E6%98%9F&fr=yfp-search-sa")
+
+    txt = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, ".trendingNow a"))
+        )
+    print(txt.get_attribute('textContent').strip())
+    txt.click()
+    time.sleep(5)
+    txt = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, ".trendingNow a"))
+        )
+    print(txt.get_attribute('textContent').strip())
+    txt.click()
+    time.sleep(5)
+except:
+    print(sys.exc_info())
 
 driver.quit()
