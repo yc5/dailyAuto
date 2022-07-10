eb8906a2e35f67e95001459e5dc219afea7ec617
yc5 <yc@xstar.me>
2022-07-10 20:34:45 +0800

    add click eight news

--- yahoo.py
+++ yahoo.py
@@ -33,8 +33,8 @@ try:
     driver.get("https://mail.yahoo.com/")
     print("setting cookie ...")
 
-    driver.add_cookie({"name": "T", "value": YAHOO_T})
-    driver.add_cookie({"name": "Y", "value": YAHOO_Y})
+    driver.add_cookie({"name": "T", "value": YAHOO_T, "domain": "yahoo.com"})
+    driver.add_cookie({"name": "Y", "value": YAHOO_Y, "domain": "yahoo.com"})
     print("redirect to yahoo mail ...")
     driver.get("https://mail.yahoo.com/")
 
@@ -88,6 +88,53 @@ try:
 except:
     print(sys.exc_info())
 
+# click eight news
+chrome_options = Options()
+chrome_options.add_argument("--window-size=720,1280")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/103.0.5060.63 Mobile/15E148 Safari/604.1"
+)
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://tw.yahoo.com/member")
+driver.add_cookie({"name": "T", "value": YAHOO_T, "domain": "yahoo.com"})
+driver.add_cookie({"name": "Y", "value": YAHOO_Y, "domain": "yahoo.com"})
+
+url_challenges_info_api = "https://tw.yahoo.com/_td-hl/api/resource/LoyaltyService.challenges?bkt=tw-fp-mweb-tts3-part2-bucket&contentType=null&device=smartphone&ecma=default&feature=always%2CnewBreakingNews%2CadFeedback%2CoathPlayer%2CuseDefaultNavigation%2CuseNTKCanonical%2Ctts3&intl=tw&lang=zh-Hant-TW&page=membercenter&pageType=undefined&partner=none&period=day&prid=988irr5hcl6bf&region=TW&site=fp&subCategory=null&tz=Asia%2FTaipei&ver=3.2.4345&returnMeta=true"
+current_execution_times = driver.execute_async_script(
+    "var callback = arguments[arguments.length-1]; return fetch('"
+    + url_challenges_info_api
+    + "').then(e=>e.json()).then(json=>json.data.challenges[1].currentExecutionTimes).then(callback).catch(callback);"
+)
+
+driver.get("https://tw.news.yahoo.com")
+links_news = WebDriverWait(driver, 5).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#ntk-list a"))
+)
+list_links_news = []
+for x in links_news:
+    list_links_news.append(x.get_attribute("href"))
+
+for x in range(8 - current_execution_times):
+    driver.get(list_links_news[x])
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    try:
+        btn_skip = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".skip-button"))
+        )
+        btn_skip.click()
+    except:
+        print(x, "there is no skip button")
+
+    btn = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".challenge-target > button"))
+    )
+    driver.execute_script(
+        "window.scrollTo(0, document.querySelector('[role=article]').offsetHeight);"
+    )
+    btn.click()
+    time.sleep(5)
 
 # line notify
 data = {"message": msg}
